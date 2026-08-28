53c74ad670fd5913918480fa289637c308d785d9
Johan Tedenmark <johan.tedenmark@hotmail.se>
2026-08-28 23:12:47 +0200

    add customers

--- src/Facades/Visma.php
+++ src/Facades/Visma.php
@@ -4,11 +4,13 @@ namespace Mezai\Visma\Facades;
 
 use Mezai\Visma\Resources\CustomerInvoice;
 use Mezai\Visma\Resources\TermsOfPayment;
+use Mezai\Visma\Resources\Customer;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static CustomerInvoice invoices()
  * @method static TermsOfPayment terms()
+ * @method static Customer customers()
  **/
 class Visma extends Facade
 {
