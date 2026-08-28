0763403344892b08e41f663b8004233594822c28
Johan Tedenmark <johan.tedenmark@hotmail.se>
2026-08-29 00:12:35 +0200

    add filters stripe and swish

--- src/Resources/TermsOfPayment.php
+++ src/Resources/TermsOfPayment.php
@@ -3,10 +3,22 @@
 namespace Mezai\Visma\Resources;
 
 use Mezai\Visma\Contracts\Resources\Crud\Readable;
+use Mezai\Visma\Contracts\Resources\Filters\FiltersFilter;
 
 class TermsOfPayment extends BaseResource
 {
     use Readable;
+    use FiltersFilter;
+
+    public function stripe(): static
+    {
+        return $this->filter("Name eq 'Stripe'");
+    }
+
+    public function swish(): static
+    {
+        return $this->filter("NameEnglish eq 'Swish direct payment'");
+    }
 
     protected function getEndpoint(): string
     {
